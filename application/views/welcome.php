ebbade38454d19f72cda7af3ee276261f8862281
PillowSky <ihyzi@163.com>
2014-05-22 14:32:41 +0800

    add viewpoint

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="renderer" content="webkit">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 	<title>有处可逃·设计创新班2014招生</title>
 	<link rel="stylesheet" href="css/plugin.css">
 	<link rel="stylesheet" href="css/style.css">
