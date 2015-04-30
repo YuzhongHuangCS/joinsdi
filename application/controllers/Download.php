c32cb3c8414124c150e5e99c86a8aebf70f8704b
Yuzo <hyzgog@gmail.com>
2015-04-30 20:28:04 +0800

    download done

---
+++ application/controllers/Download.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Download extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+		$this->load->helper('download');
+	}
+
+	public function index() {
+		force_download(FCPATH . '/static/2014级设计创新班招生报名表.docx', NULL);
+	}
+
+}
