b9720de7a1122e08098b9124934f7b17a4338fa6
Yuzo <hyzgog@gmail.com>
2015-05-04 15:13:07 +0800

    visitorID cookie

--- application/config/constants.php
+++ application/config/constants.php
@@ -84,3 +84,10 @@ define('EXIT_USER_INPUT', 7); // invalid user input
 define('EXIT_DATABASE', 8); // database error
 define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+/*
+|--------------------------------------------------------------------------
+| Time constants
+|--------------------------------------------------------------------------
+*/
+define('SECONDS_YEAR', 31536000);
--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,7 +3,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
+	public function __construct() {
+		parent::__construct();
+		$this->load->library('encryption');
+		$this->load->library('user_agent');
+		$this->load->model('visitor');
+	}
+
 	public function index() {
+		$visitorID = $this->encryption->decrypt($this->input->cookie('visitorID'));
+
+		if ($visitorID) {
+			$this->visitor->again($visitorID);
+		} else {
+			$visitorID = $this->visitor->create($this->agent->referrer(), $this->agent->agent_string());
+			$this->input->set_cookie('visitorID', $visitorID, SECONDS_YEAR);
+		}
+
 		$this->load->view('home.html');
 	}
 }
