097a130418eeb53f7cbb5f3160669160be45d453
Yuzo <hyzgog@gmail.com>
2015-05-04 16:34:58 +0800

    renames

--- application/models/Submit.php
+++ application/models/Submit.php
@@ -10,19 +10,19 @@ class Submit extends CI_Model {
 
 	public function form($visitorID, $row) {
 		$sql = 'INSERT INTO `submit`(`visitorID`, `name`, `num`, `birthday`, `gender`, `category`, `major`, `gpa`, `rank`, `phone`, `email`, `dormitory`, `remark`, `social`, `workshop`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-		$data = [$visitorID, $row['name'], $row['num'], $row['birthday'], $row['gender'], $row['category'], $row['major'], $row['gpa'], $row['rank'], $row['phone'], $row['email'], $row['dormitory'], $row['remark'], $row['social'], implode(',', $row['workshop'])];
+		$data = [$visitorID, $row['name'], intval($row['num']), $row['birthday'], $row['gender'], $row['category'], $row['major'], $row['gpa'], $row['rank'], $row['phone'], $row['email'], $row['dormitory'], $row['remark'], $row['social'], implode(',', $row['workshop'])];
 		$this->db->query($sql, $data);
 		return $this->db->insert_id();
 	}
 
-	public function avatar($submitID, $file) {
-		$sql = 'UPDATE `submit` SET `avator` = ? WHERE `submitID` = ?';
-		return $this->db->query($sql, [$file, $submitID]);
+	public function avatar($ID, $file) {
+		$sql = 'UPDATE `submit` SET `avator` = ? WHERE `ID` = ?';
+		return $this->db->query($sql, [$file, $ID]);
 	}
 
-	public function apply($submitID, $file) {
-		$sql = 'UPDATE `submit` SET `apply` = ? WHERE `submitID` = ?';
-		return $this->db->query($sql, [$file, $submitID]);
+	public function apply($ID, $file) {
+		$sql = 'UPDATE `submit` SET `apply` = ? WHERE `ID` = ?';
+		return $this->db->query($sql, [$file, $ID]);
 	}
 
 }
